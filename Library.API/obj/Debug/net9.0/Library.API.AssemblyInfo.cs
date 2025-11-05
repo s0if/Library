50bff17fd9e18c8199c1c1f241a7b433b9c0515e
s0if <saifalkomi@gmail.com>
2025-11-05 14:37:50 +0200

    edit readme

--- Library.API/obj/Debug/net9.0/Library.API.AssemblyInfo.cs
+++ Library.API/obj/Debug/net9.0/Library.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6ef75d62eb4c8a1d4ce091c0bdb05cfb0b9cce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21e543d47a8c666ccfa7a57a5d6d5ef88060a8c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
